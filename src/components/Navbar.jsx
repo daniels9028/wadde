452aa18c85d45f5ec058378083f362f7575114f9
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-09 13:16:12 +0700

    feat: Repair navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
+import { FiAlignRight } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="fixed w-full left-0 top-0 bg-white shadow-md">
       <div className="container mx-auto h-20 lg:px-10 px-4 flex flex-row justify-between items-center">
@@ -9,15 +13,21 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-6 h-6" />
           <h1 className="text-lg font-bold">Wadde</h1>
         </div>
-        <ul className="lg:flex hidden flex-row items-center gap-8 cursor-pointer font-semibold tracking-wide">
+        <ul className="lg:flex hidden flex-row items-center gap-8 cursor-pointer font-medium tracking-wide">
           <li>All Pages</li>
           <li>Pricing</li>
           <li>Contact</li>
           <li>About</li>
         </ul>
-        <button className="bg-[#F15757] text-white rounded-full py-2 px-8 tracking-wide hover:bg-white hover:text-black border-2 hover:border-[#F15757] transition-all duration-200">
+        <button className="bg-[#F15757] text-white rounded-full py-2 px-8 tracking-wide hover:bg-white hover:text-black border-2 hover:border-[#F15757] transition-all duration-200 lg:block hidden">
           Sign In
         </button>
+        <div
+          className="lg:hidden block text-3xl cursor-pointer transition-all"
+          onClick={() => setNav(!nav)}
+        >
+          {nav ? <FiX color="black" /> : <FiAlignRight color="black" />}
+        </div>
       </div>
     </div>
   );
