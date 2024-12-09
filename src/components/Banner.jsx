32d1362f7d000e2fb7e228f2c769907a1777bb6c
daniels9028 <daniels9028@student.untan.ac.id>
2024-12-09 16:40:29 +0700

    Banner Responsive Slicing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,29 +7,29 @@ import banner5 from "../assets/banner5.png";
 
 const Banner = () => {
   return (
-    <section className="mt-24">
-      <div className="container mx-auto flex lg:flex-row flex-col-reverse justify-between items-center gap-4 lg:pl-10 pl-4 lg:pr-0 pr-4">
-        <div className="flex items-center flex-wrap lg:w-1/2 w-full">
+    <section className="my-24">
+      <div className="container mx-auto flex lg:flex-row flex-col-reverse justify-between items-center gap-4 lg:pl-10 pl-4">
+        <div className="flex items-center flex-wrap lg:w-1/2 w-full space-x-2 lg:pr-0 pr-4">
           <p className="font-bold text-[50px]">Intelligent</p>
           <img
             src={banner1}
             alt="banner1"
             className="w-28 h-28 object-contain"
           />
-          <p className="font-bold text-[50px]">Tools for</p>
+          <p className="font-bold text-[50px]">Tools</p>
+          <p className="font-bold text-[50px]">for</p>
           <img
             src={banner2}
             alt="banner2"
             className="w-28 h-28 object-contain"
           />
-          <p className="font-bold text-[50px] drop-shadow-md">
-            Savvy Businesses
-          </p>
+          <p className="font-bold text-[50px] drop-shadow-md">Savvy</p>
+          <p className="font-bold text-[50px] drop-shadow-md">Businesses</p>
           <p className="text-2xl mt-6 text-[#1C1C1C]/60 font-medium">
             FramerBite provides the essential elements for crafting a top-tier
             website for your SaaS platform.
           </p>
-          <div className="mt-20 flex lg:flex-row flex-col  gap-8 items-center">
+          <div className="lg:mt-20 mt-10 flex lg:flex-row flex-col gap-8 lg:justify-between justify-center w-full items-center">
             <button className="bg-[#F15757] py-4 px-6 rounded-full text-white hover:bg-white hover:text-black border-2 hover:border-[#F15757] transition-all duration-200">
               Get Started Now
             </button>
@@ -47,10 +47,10 @@ const Banner = () => {
               <span className="w-16 h-16 rounded-full bg-white border border-gray-400 text-5xl -ml-3 z-[4] flex justify-center items-center">
                 +
               </span>
-              <div className="flex flex-col justify-center ml-8">
-                <p className="font-bold text-[36px]">3.2m</p>
-                <p className="text-[18px] text-[#1C1C1C]/60">Success clients</p>
-              </div>
+            </div>
+            <div className="flex flex-col justify-center ml-8 text-center">
+              <p className="font-bold text-[36px]">3.2m</p>
+              <p className="text-[18px] text-[#1C1C1C]/60">Success clients</p>
             </div>
           </div>
         </div>
